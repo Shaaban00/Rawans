6f2d2c454316484849c089ad6b2d1a076bfb6f54
Shaaban00 <m.shaaban.m0@gmail.com>
2025-02-13 20:49:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Rawraw",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine? 🥰",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -23,9 +23,9 @@ const CONFIG = {
     questions: {
         first: {
             text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            yesBtn: "Yes🤩",                                             // Text for "Yes" button
+            noBtn: "No🥀",                                               // Text for "No" button
+            secretAnswer: "Bethbeni? Tab3an Ya Hayati 🥰"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -34,23 +34,23 @@ const CONFIG = {
         },
         third: {
             text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            yesBtn: "Yes!🤩",                                             // Text for "Yes" button
+            noBtn: "No🥀"                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "YANHAR ABYAD, AD L BA7R W SAMAKATUU??? 🥰🤩💝",  // Shows when they go past 5000%
+        high: "LA ANA BAHEBIKKK AKTARR 🤩💝",              // Shows when they go past 1000%
+        normal: "3LFKRA FI 7OB AKTAR MN KDA 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! Can’t wait to spend Valentine’s with you! 🎉💝💖💝💓",
+        message: "Get over here, babe. I have a hug for you, and maybe something that'll leave you breathless.🤤🤤😏",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
